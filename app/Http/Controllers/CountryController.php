78808fd01dde3c7b6c0c23b1fcb68d0286dd180d
Aniket <anidhatrak@gmail.com>
2025-02-27 12:36:41 +0530

    Add country dashboard and user count by state functionality

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Country;
 
 class CountryController extends Controller
 {
-    //
+    public function country(){
+        $defaultCountry= Country::where('name', 'India')->first();
+        return view('country-dashboard', compact('defaultCountry'));
+    }
+
+    public function getUsersByState(Request $request){
+        $countryId = $request->input('country_id');
+
+        $data = User::where('state_id, count(*) as user_count')
+            ->where('country_id', $countryId)
+            ->groupBy('state_id')
+            ->with('state')
+            ->get();
+
+        return response()->json($data);
+    }
 }
--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-        {{ __('Country') }}
+            {{ __('Country') }}
         </h2>
     </x-slot>
 
@@ -9,22 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                      <canvas id="myChart"></canvas>
+                    <input type="hidden" id="defaultCountryId" value="{{ $defaultCountry->id }}">
+                    <canvas id="stateChart"></canvas>
                 </div>
             </div>
         </div>
     </div>
 
     <script>
-        const ctx = document.getElementById('myChart');
+        const ctx = document.getElementById('stateChart');
 
         new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: 'users per state',
+                    data: data,
                     borderWidth: 1
                 }]
             },
